ca2680b3fe4bbf589b9e76ebcdfee15985f32b01
Jake Di Toro <karrde@viluppo.net>
2014-05-20 19:42:56 -0400

    fix for redirect on root url

--- ffg_swrpg/urls.py
+++ ffg_swrpg/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ffg_swrpg.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url='books/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^books/', include('books.urls', namespace='books')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
